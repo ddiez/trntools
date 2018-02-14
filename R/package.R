19dc17ff1918c8c487379c144177912e33390455
Diego Diez <diego10ruiz@gmail.com>
2018-02-14 18:20:23 +0900

    Add package.R file with dependencies. Added to DESCRIPTION.

---
+++ R/package.R
@@ -0,0 +1,12 @@
+#' Tools for the analysis of transcriptional regulatory networks
+#'
+#' Tools for the analysis of transcriptional regulatory networks
+#'
+#' @name trntools
+#' @docType package
+#'
+#' @import ggplot2 tidygraph ggraph
+#' @importFrom igraph graph_from_incidence_matrix
+#' @importFrom stats coef lm
+#'
+NULL
